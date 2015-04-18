3a4cb7e16e4ad6f6ec3a2602a5e78338129e1947
Kit <kittsville@googlemail.com>
2015-04-18 21:42:20 +0100

    Added basic test data page

--- lib/wp-librarian-test.class.php
+++ lib/wp-librarian-test.class.php
@@ -47,6 +47,54 @@ class WP_LIBRARIAN_TEST {
 	 * @param	WP_LIB_AJAX_PAGE	$ajax_page	Instance of plugin AJAX page creating class
 	 */
 	public function addTestDataPage(WP_LIB_AJAX_PAGE $ajax_page) {
-		$ajax_page->sendPage( 'Test Data', 'Test Data', array());
+		$page = array(
+			array(
+				'type'		=> 'paras',
+				'content'	=> array(
+					'Generate or delete test data'
+				)
+			)
+		);
+		
+		$form[] = $ajax_page->prepNonce('Managing Test Data');
+		
+		// Looks for any objects marked as test data
+		$query = new WP_Query(array(
+			'post_type'		=> 'wp_lib_items',
+			'post_status'	=> 'publish',
+			'meta_query'	=> array(
+				array(
+					'key'		=> 'wp_lib_test_data',
+					'compare'	=> 'EXISTS'
+				)
+			)
+		));
+		
+		// If there is no test data, allow for its creation
+		if (!$query->have_posts()) {
+		$form[] = array(
+			'type'	=> 'button',
+			'link'	=> 'action',
+			'value'	=> 'gen-test-data',
+			'html'	=> 'Generate',
+			'title'	=> 'Create all fixture data'
+		);
+		} else {
+			$form[] = array(
+				'type'		=> 'button',
+				'link'		=> 'action',
+				'value'		=> 'delete-test-data',
+				'classes'	=> 'dash-button-danger',
+				'html'		=> 'Delete',
+				'title'		=> 'Delete all fixture data'
+			);
+		}
+		
+		$page[] = array(
+			'type'		=> 'form',
+			'content'	=> $form
+		);
+		
+		$ajax_page->sendPage( 'Test Data Management Panel', 'Test Data', $page);
 	}
 }
