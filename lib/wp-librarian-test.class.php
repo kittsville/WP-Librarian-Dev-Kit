f7feee4d0b3f273a0787c9b3eeb13f60fd317cf7
Kit <kittsville@googlemail.com>
2015-04-18 15:28:58 +0100

    Added Dash home button
    Added button to dash home that links to the page to manage test data
    Improved comments

--- lib/wp-librarian-test.class.php
+++ lib/wp-librarian-test.class.php
@@ -15,18 +15,26 @@ class WP_LIBRARIAN_TEST {
 	 * Registers WP-Librarian hooks
 	 */
 	function __construct(){
-		add_filter('wp_lib_dash_page',		array($this, 'addTestDataButton'), 10, 2);
+		add_filter('wp_lib_dash_home_buttons',		array($this,	'addTestDataButton'),	10, 2);
 	}
 	
 	/**
-	 * Adds a button to the library Dashboard to visit the test data page
-	 * @param	array	$page		Complete Dash page
-	 * @param	string	$dash_page	The page's identifying string e.g. 'pay-fines'
+	 * Adds a button to the library Dashboard for lib admins to visit the test data page
+	 * @param	array	$buttons	Dashboard buttons
+	 * @return	array				Dash buttons with Test Data button added
 	 */
-	public function addTestDataButton(Array $page, $dash_page) {
-		if ($dash_page !== 'dashboard')
-			return $page;
+	public function addTestDataButton(Array $buttons) {
+		if (!wp_lib_is_library_admin())
+			return $buttons;
 		
-		return $page;
+		$buttons[] = array(
+			'bName'	=> 'Test Data',
+			'icon'	=> 'admin-tools',
+			'link'	=> 'dash-page',
+			'value'	=> 'test-data',
+			'title'	=> 'Generate or delete test data'
+		);
+		
+		return $buttons;
 	}
 }
